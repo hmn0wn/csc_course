f83ab3e3a4ff95dc9a0b43f289f29217714df2df
leadenHoar <eskendirrr@gmail.com>
2014-04-15 21:36:33 +0400

    Класс server полностью реализован.

--- Study/PracticeConsoleApplications/OOPWork/Source.cpp
+++ Study/PracticeConsoleApplications/OOPWork/Source.cpp
@@ -12,6 +12,12 @@ public:
 
 class computer_screen : public comp_component{
 public:
+	computer_screen(){
+		strcpy(type, "");
+		colors = 0;
+		x_resolution = 0;
+		y_resolution = 0;
+	}
 	computer_screen(char *, long, int, int);
 	void show_properties(void);
 
@@ -37,6 +43,11 @@ void computer_screen::show_properties(void){
 
 class mother_board : public comp_component{
 public:
+	mother_board(){
+		processor = 0;
+		speed = 0;
+		RAM = 0;
+	}
 	mother_board(int, int, int);
 	void show_properties(void);
 
@@ -58,45 +69,68 @@ void mother_board::show_properties(void){
 	cout << "RAM:\t" << RAM << " Mb" << endl;
 }
 
-class computerObj :public computer_screen, public mother_board{
-public:
-	computerObj(char *, int, char *, long, int, int, int, int, int);
-	void show_properties();
-private:
-	char name[64];
-};
-
-/*computerObj::computerObj(char *name, char *screen_type, long colors, int x_res, int y_res, int processor, int speed, int ram) :
-computer_screen(screen_type, colors, x_res, y_res), mother_board(processor, speed, ram){
-	strcpy(computerObj::name, name);
-	
-}
-*/
-
 class computer : public computer_screen, public mother_board{
 
 public:
-	computer(char *, int, char *, long, int, int, int, int, int);
+	computer();
+	computer(int, char *, long, int, int, int, int, int);
 	void show_properties(void);
 
 private:
-	char name[64];
-	int hard_disc;
-	
+	int hard_disc;	
 };
+computer::computer(){
+	hard_disc = 0;
+}
 
-computer::computer(char *name, int hard_disc, char *screen_type, long colors, int x_res, int y_res, int processor, int speed, int ram) :
+computer::computer(int hard_disc, char *screen_type, long colors, int x_res, int y_res, int processor, int speed, int ram) :
 computer_screen(screen_type, colors, x_res, y_res), mother_board(processor, speed, ram){
-	strcpy(computer::name, name);
 	computer::hard_disc = hard_disc;
 	
 }
 
 void computer::show_properties(void){
-	cout << "Type of computer:\t" << name << endl;
-	cout << "Hard drive:\t" << hard_disc << endl;
 	computer_screen::show_properties();
 	mother_board::show_properties ();
+	cout << "Hard drive:\t" << hard_disc << endl;
+}
+
+class pc :public computer{
+
+public:
+	pc(int, int, char *, long, int, int, int, int, int);
+	void show_properties(void);
+private:
+	int mouse_dpi;
+};
+
+pc::pc(int m_dpi, int hard_disc, char *screen_type, long colors, int x_res, int y_res, int processor, int speed, int ram) :
+computer(hard_disc, screen_type, colors, x_res, y_res, processor, speed, ram){
+pc:mouse_dpi = m_dpi;
+}
+
+void pc::show_properties(void){
+	computer::show_properties();
+	cout << "Mouse dpi:\t" << mouse_dpi << endl;
+}
+
+class laptop :public computer{
+
+public:
+	laptop(bool, int, char *, long, int, int, int, int, int);
+	void show_properties(void);
+private:
+	bool touchpad;
+};
+
+laptop::laptop(bool tch, int hard_disc, char *screen_type, long colors, int x_res, int y_res, int processor, int speed, int ram) :
+computer(hard_disc, screen_type, colors, x_res, y_res, processor, speed, ram){
+	laptop:touchpad = tch;
+}
+
+void laptop::show_properties(void){
+	computer::show_properties();
+	cout << "Touchpad:\t" << ((touchpad) ? "yes" : "no") << endl;
 }
 
 #pragma region List
@@ -172,19 +206,23 @@ size_t List::getcount(){
 const size_t SIZE = 10;
 template <class T>
 class _array{
-	T element[SIZE];
-	size_t count;
 public:
-	_array(){
-		for (size_t i = 0; i < count; i++)
-			element[i] = NULL;
-		count = 0;
-	}
+	T element[SIZE];
+	_array();
 	void add(T value);
-	void print_all();
+	virtual void print_all();
 	void clean();
-	
+	size_t getcount();
+private:
+	size_t count;
 };
+
+template <class T>
+_array<T>::_array(){
+	/*for (size_t i = 0; i < SIZE; i++)
+		element[i] = NULL;*/
+	count = 0;
+}
 
 template <class T>
 void _array<T>::add(T value){
@@ -207,33 +245,51 @@ void _array<T>::clean(){
 		element[i] = NULL;
 	count = 0;
 }
+
+template <class T>
+size_t _array<T>::getcount(){
+	return count;
+}
 #pragma endregion
 
 
+class server : public computer, public _array <computer>{
+	
+public:
+	server(char *, int, char *, long, int, int, int, int, int);
+	void print_all();
+
+private:
+	char ip[16];
+};
+
+server::server(char *ip, int hard_disc, char *screen_type, long colors, int x_res, int y_res, int processor, int speed, int ram) :
+computer(hard_disc, screen_type, colors, x_res, y_res, processor, speed, ram)/*, _array<computer>::_array()*/{
+	strcpy(server::ip, ip);
+}
+
+void server::print_all(){
+	for (size_t i = 0; i < getcount(); i++)
+		element[i].show_properties();
+}
+
+
 int main(){
-	computer my_pc("PC", 128, "VGA", 256, 512, 512, 86, 2000, 1024);
-	my_pc.show_properties();
-	cout << endl;
-	
-	List mylist;
-	_array<int> myarray;
-	
+	computer my_pc(128, "VGA", 256, 512, 512, 86, 2000, 1024);
+	//my_pc.show_properties();
+	//cout << endl;
 
-	for (size_t i = 0; i < 10; i++){
-		mylist.appendNode(i);
-		myarray.add(i);
-	}
-
-
-	mylist.print_all();
-	mylist.clean();
-	mylist.print_all();
+	server my_server("128.0.0.1", 512, "DVI", 65324, 1920, 1080, 86, 4000, 8192);
+	my_server.show_properties();
+	my_server.add(my_pc);
 
 	cout << endl;
+	my_server.print_all();
+	
 
-	myarray.print_all();
-	myarray.clean();
-	myarray.print_all();
+	
+
+	
 
 
 	return 0;
