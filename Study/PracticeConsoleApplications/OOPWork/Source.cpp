8380886a95f8338d8df383fd5f04d5c12197dc30
leadenHoar <eskendirrr@gmail.com>
2014-04-16 02:20:09 +0400

    Добавлены комментарии и help.

--- Study/PracticeConsoleApplications/OOPWork/Source.cpp
+++ Study/PracticeConsoleApplications/OOPWork/Source.cpp
@@ -6,11 +6,18 @@
 #include <string.h>
 using namespace std;
 
+//Базовый, абстрактный класс, содержит только один чисто виртуальный метод, 
+//символизирующий абстрактный компоненте в составе компьютера.
+
 class comp_component{
 public:
-	virtual ostream &show_properties(ostream &stream) = 0;
+	virtual ostream &show_properties(ostream &stream) = 0;//Подобное определение и есть пример динамического полиморфизма.
 };
 
+//Наследник базового класса, содержит определение виртуальной функции предка,
+//которая выводит в поток значения полей класса.
+//Класс символизирует монитор компьютера, содержит поля: тип, колличество цветов,
+//и разрешение.
 class computer_screen : public comp_component{
 public:
 	computer_screen(){
@@ -43,6 +50,9 @@ ostream &computer_screen::show_properties(ostream &stream){
 	return stream;
 }
 
+//Второй наследник базового класса, содержит определение виртуальной функции(аналогичное),
+//Символизирует материнскую плату компьютера, содержит поля: тип процессора, 
+//частота процессора, объем опереативной памяти; содержит процедуру вывода параметров.
 class mother_board : public comp_component{
 public:
 	mother_board(){
@@ -72,6 +82,10 @@ ostream & mother_board::show_properties(ostream &stream){
 	return stream;
 }
 
+//Класс компьютера, являет собой результат множественного наследования от классов:
+//материнской платы и монитора, содержит в себе все их поля и аналогичную процедуру
+//вывода параметров в поток, которая доопределяется; она определена в базовом классе
+//как виртуальная функция
 class computer : public computer_screen, public mother_board{
 
 public:
@@ -99,6 +113,8 @@ ostream &computer::show_properties(ostream &stream){
 	return stream;
 }
 
+//Более узкий класс, обычный пк, наследуется от компьютера, добавлено одно поле, и немного
+//доопределена процедура вывода.
 class pc :public computer{
 
 public:
@@ -119,6 +135,7 @@ ostream & pc::show_properties(ostream &stream){
 	return stream;
 }
 
+//Аналогично только лаптоп.
 class laptop :public computer{
 
 public:
@@ -139,6 +156,7 @@ ostream & laptop::show_properties(ostream &stream){
 	return stream;
 }
 
+//Не нужнет.
 #pragma region List
 class List{
 	
@@ -208,8 +226,10 @@ size_t List::getcount(){
 }
 #pragma endregion 
 
+//Шаблонный класс обобщенного массива.
 #pragma region TemplateArray
-const size_t SIZE = 10;
+const size_t SIZE = 10;// Максимальное число элементов массива, архитектурно вынужденное решение, определяется пользователем в коде программы
+//никак не контроллируется извне программы, ибо должно быть определено еще на этапе компилляции, причина тому — статический массив.
 template <class T>
 class _array{
 public:
@@ -258,6 +278,12 @@ size_t _array<T>::getcount(){
 }
 #pragma endregion
 
+//Апогей творения, сервер, наследуется от массива и компьютера;
+//наследование от компьютера дает поля и метод вывода полей;
+//наследование от массива(с уточнением типа, на компьютер) дает поле типа массива
+//и методы для работы с этим полем; это поле может содержать как пк так и лаптопы, ибо класс предок у них 
+//один и он же и уточняет тип на этапе наследования;
+
 
 class server : public computer, public _array <computer>{
 	
@@ -274,6 +300,9 @@ computer(hard_disc, screen_type, colors, x_res, y_res, processor, speed, ram)/*,
 	strcpy(server::ip, ip);
 }
 
+//Ксасс сервер прегружает метод массива отвечающий за вывод в поток параметров всех содержащихся в нем классав,
+//классов типа компьютера, а именно лаптопы и пк, которые в свою очередь имеют собственные методы вывода информации
+//в поток stream.
 ostream &server::print_all(ostream &stream){
 	for (size_t i = 0; i < getcount(); i++){
 		stream << "Comp[" << i + 1 << "]:" << endl;
@@ -284,6 +313,9 @@ ostream &server::print_all(ostream &stream){
 	return stream;
 }
 
+//Перегрузка оперетора вывода в поток, из стандартной библиотеки ввода вывода <iostream>.
+//За основу берется уже реализованный метод вывода в поток у всех реализованных классов.
+//Это обстоятельсто гарантируется виртуальным методом базового абстрактного класса.
 ostream &operator<<(ostream &stream, server serv){
 	serv.print_all(stream);
 	return stream;
@@ -291,72 +323,86 @@ ostream &operator<<(ostream &stream, server serv){
 
 
 
-int main(){
-	//computer my_pc(128, "VGA", 256, 512, 512, 86, 2000, 1024);
-	//my_pc.show_properties();
-	//cout << endl;
+void main(int argc, char *argv[]){
+	if (argc == 2 && !strcmp(argv[1], "help")){
+		/*cout << "Запуск программы происходит при использовании параметра командной стрки \"run\"" << endl;
+		cout << "Данные для демонстрационного примера берутся из файла input.txt." << endl;
+		cout << "В первой строке данные для заполнения полей сервера, он один, определен программно." << endl;
+		cout << "Во второй колличесто компьютеров для ввода в сервер, не должно превосходить SIZE." << endl;
+		cout << "Далее следуют текстовые специффикаторы pc и laptop." << endl;
+		cout << "А за ними соответствеющие данные." << endl;*/
 
-	//server my_server("128.0.0.1", 512, "DVI", 65324, 1920, 1080, 86, 4000, 8192);
-	//my_server.show_properties();
-	//my_server.add(my_pc);
-
-	//cout << endl;
-	//cout << my_server;
-	//my_server.print_all();
-
-
-	
-
-	char ip[65];
-	int hard_disc;
-	char screen_type[65];
-	long colors;
-	int x_res;
-	int y_res;
-	int processor;
-	int speed;
-	int ram;
-	int comp_count;
-
-	char comp_type[10];
-
-	int m_dpi;
-	bool tch;
-	
-	
-	
-	ifstream in("input.txt");
-	if (!in){
-		cout << "Can not open file." << endl;
+		cout << endl << endl << "\tATTENTION!!!" << endl;
+		cout << "Run occurs when you use the command line \" run \"" << endl;
+		cout << "Data for the demo, taken from the file input.txt" << endl;
+		cout << "The first line of data to populate the server, server is defined in program code" << endl;
+		cout << "The second Nuber computers for input to the server, should not exceed SIZE" << endl;
+		cout << "Followed by text parameters pc and laptop" << endl;
+		cout << "And behind them the relevant data" << endl;
+		cout << endl;
 	}
-	else{
-		in >> ip >> hard_disc >> screen_type >> colors >> x_res >> y_res >> processor >> speed >> ram;
-		server serv(ip, hard_disc, screen_type, colors, x_res, y_res, processor, speed, ram);
-		
-		computer tmpcomp;
-		in >> comp_count;
-		for (int i = 0; i < comp_count; i++){
-			in >> comp_type;
-			if (!strcmp(comp_type, "pc")){
-				in >> m_dpi >> hard_disc >> screen_type >> colors >> x_res >> y_res >> processor >> speed >> ram;
-				tmpcomp = *(new pc(m_dpi, hard_disc, screen_type, colors, x_res, y_res, processor, speed, ram));
-				serv.add(tmpcomp);
-			}
-			if (!strcmp(comp_type, "laptop")){
-				in >> tch >> hard_disc >> screen_type >> colors >> x_res >> y_res >> processor >> speed >> ram;
-				tmpcomp = *(new laptop(tch, hard_disc, screen_type, colors, x_res, y_res, processor, speed, ram));
-				serv.add(tmpcomp);
-			}
+	else if (argc == 2 && !strcmp(argv[1], "run")){
 
+
+		char ip[65];
+		int hard_disc;
+		char screen_type[65];
+		long colors;
+		int x_res;
+		int y_res;
+		int processor;
+		int speed;
+		int ram;
+		int comp_count;
+
+		char comp_type[10];
+
+		int m_dpi;
+		bool tch;
+
+
+
+		ifstream in("input.txt");
+		if (!in){
+			cout << "Can not open file." << endl;
 		}
-		cout << "Server: " << endl;
-		serv.show_properties(cout);
+		else{
+			in >> ip >> hard_disc >> screen_type >> colors >> x_res >> y_res >> processor >> speed >> ram;
+			server serv(ip, hard_disc, screen_type, colors, x_res, y_res, processor, speed, ram);
 
-		cout << endl << serv;
+			computer tmpcomp;
+			in >> comp_count;
+
+			//Далее следует заполнение сервера компьютерами.
+			//Это классический пример динамического полиморфизма, посылки к такому его применению положены
+			//в классе компьютер который является предком классов пк и лаптоп.
+			//По текстовому специффикатору происходит заполнение массива соответствующими элементами, тип которых
+			//выбирается динамически, но насмотря на это --->
+			for (int i = 0; i < comp_count; i++){
+				in >> comp_type;
+				if (!strcmp(comp_type, "pc")){
+					in >> m_dpi >> hard_disc >> screen_type >> colors >> x_res >> y_res >> processor >> speed >> ram;
+					tmpcomp = *(new pc(m_dpi, hard_disc, screen_type, colors, x_res, y_res, processor, speed, ram));
+					serv.add(tmpcomp);
+				}
+				if (!strcmp(comp_type, "laptop")){
+					in >> tch >> hard_disc >> screen_type >> colors >> x_res >> y_res >> processor >> speed >> ram;
+					tmpcomp = *(new laptop(tch, hard_disc, screen_type, colors, x_res, y_res, processor, speed, ram));
+					serv.add(tmpcomp);
+				}
+
+			}
+			
+			cout << "Server: " << endl;
+			serv.show_properties(cout);
+			//--->несмотря на это, метод для вывода разнотипных элементов один и тот же,
+			//он реализован еще в классе предке, в компьютере, а объявлен как виртуальный, еще в базовом
+			//классе
+			cout << endl << serv;
+		}
+
+		in.close();
 	}
-
-	in.close();
-
 	
-	return 0;
+	
 }
