327c8565d136dedd4b4044c579828af1eca1f473
leadenHoar <eskendirrr@gmail.com>
2014-05-09 20:16:00 +0400

    Дебажу бин поиск.

--- Study/PracticeConsoleApplications/Ex4.4.1(Points_Segments)/Source.cpp
+++ Study/PracticeConsoleApplications/Ex4.4.1(Points_Segments)/Source.cpp
@@ -9,12 +9,12 @@ using namespace std;
 class Queue_{//Очередь, че. Пишу как представляю.
 private:
 	struct node{
-		size_t *data;
+		int *data;
 		char *shadow_data;
 		size_t length;
 		node *next;
 		node *prev;
-		node(size_t *dat, char *sh_dat, size_t len) :data(dat), shadow_data(sh_dat), length(len), next(NULL), prev(NULL){}
+		node(int *dat, char *sh_dat, size_t len) :data(dat), shadow_data(sh_dat), length(len), next(NULL), prev(NULL){}
 		ostream &print_data(ostream &stream){
 			stream << "[";
 			for (size_t i = 0; i < length-1; i++)
@@ -40,14 +40,14 @@ public:
 	node *tail;
 
 	Queue_();
-	void enqueue(size_t *dat, char *sh_dat, size_t len);//В очередь.
-	void dequeue(size_t **dat, char **sh_dat, size_t &len);//Из очереди.
+	void enqueue(int *dat, char *sh_dat, size_t len);//В очередь.
+	void dequeue(int **dat, char **sh_dat, size_t &len);//Из очереди.
 	ostream &print(ostream &stream);
 	ostream &operator<<(node *n);
 };
 Queue_::Queue_():length(0),head(NULL),tail(NULL){}
 
-void Queue_::enqueue(size_t *dat, char *sh_dat, size_t len){
+void Queue_::enqueue(int *dat, char *sh_dat, size_t len){
 	node *tmp = new node(dat, sh_dat, len);
 	if (!tail){
 		head = tmp;
@@ -61,7 +61,7 @@ void Queue_::enqueue(size_t *dat, char *sh_dat, size_t len){
 	length++;
 }
 
-void Queue_::dequeue(size_t **dat, char **sh_dat, size_t &len){
+void Queue_::dequeue(int **dat, char **sh_dat, size_t &len){
 	if (head){
 		node *tmp = head;
 		*dat = tmp->data;
@@ -128,21 +128,22 @@ size_t length_(size_t *b, size_t *e){
 
 #pragma endregion
 
-void merge_sort(size_t **data, char **shadow_data, size_t length){
+void merge_sort(int **data, char **shadow_data, size_t length){
 	Queue_ merge_queue;
 
-	size_t *tmp;
+	int *tmp;
 	char *sh_tmp;
 	for (size_t i = 0; i < length; i++){
-		tmp = new size_t[1];
+		tmp = new int[1];
 		sh_tmp = new char[1];
 		tmp[0] = (*data)[i];
 		sh_tmp[0] = (*shadow_data)[i];
 		merge_queue.enqueue(tmp, sh_tmp, 1);
-		merge_queue.print(cout);
+		//merge_queue.print(cout);
 	}
 
-	size_t *dat1, len1, *dat2, len2, i, j, k;
+	int *dat1, *dat2;
+	size_t len1, len2, i, j, k;
 	char *sh_dat1, *sh_dat2;
 
 	while (merge_queue.head->prev){
@@ -150,19 +151,20 @@ void merge_sort(size_t **data, char **shadow_data, size_t length){
 		merge_queue.dequeue(&dat2,&sh_dat2,len2);
 
 		
-		print(cout, "   dat1", dat1, len1);
-		print(cout, "sh_dat1", sh_dat1, len1);
-		print(cout, "   dat2", dat2, len2);
-		print(cout, "sh_dat2", sh_dat2, len2);
+		//print(cout, "   dat1", dat1, len1);
+		//print(cout, "sh_dat1", sh_dat1, len1);
+		//print(cout, "   dat2", dat2, len2);
+		//print(cout, "sh_dat2", sh_dat2, len2);
 		
 		i = 0;
 		j = 0;
 		k = 0;
-		tmp = new size_t[len1 + len2];
+		tmp = new int[len1 + len2];
 		sh_tmp = new char[len1 + len2];
 
 
 		while (i < len1 || j < len2){
+			//cout << "i: " << i << endl << "j: "<< j << endl;
 			if (i == len1){
 				tmp[k] = dat2[j];
 				sh_tmp[k++] = sh_dat2[j++];
@@ -181,7 +183,7 @@ void merge_sort(size_t **data, char **shadow_data, size_t length){
 
 			}
 			else{//Случай равенства по значению.
-				if (dat1[i] == dat2[i]){//Точки должны быть внутри отрезков, даже, если они на границе, и плюс стабильность(одинаковые элементы сохраняют порядок следования).
+				if (dat1[i] == dat2[j]){//Точки должны быть внутри отрезков, даже, если они на границе, и плюс стабильность(одинаковые элементы сохраняют порядок следования).
 					if (sh_dat1[i] == '{'){
 						tmp[k] = dat1[i];
 						sh_tmp[k++] = sh_dat1[i++];
@@ -224,14 +226,39 @@ void merge_sort(size_t **data, char **shadow_data, size_t length){
 		//Теперь очистим память массивов с которыми работали, и запишем в очереть новый массив, из них составленный.
 		delete(dat1);
 		delete(dat2);
-		print(cout, "   tmp", tmp, len1 + len2);
-		print(cout, "sh_tmp", sh_tmp, len1 + len2);
+		//print(cout, "   tmp", tmp, len1 + len2);
+		//print(cout, "sh_tmp", sh_tmp, len1 + len2);
 		merge_queue.enqueue(tmp,sh_tmp, len1+len2);
-		merge_queue.print(cout);
+		//merge_queue.print(cout);
 	}
 
 	*data = merge_queue.head->data;
 	*shadow_data = merge_queue.head->shadow_data;
+}
+
+size_t bin_search(int *data, char *shadow_data, size_t length, int point){
+	
+	size_t begin =0, mid = 0, end = length-1;
+	
+	while ((data[mid] != point || shadow_data[mid] != '.') && begin<=end)
+	{
+		length = length / 2;
+		mid = begin + length;
+		
+		//cout << " begin: " << begin << endl;	
+		//cout << "   mid: " << mid << endl;
+		//cout << "   end: " << end << endl;
+		//cout << "length: " << length << endl<<endl;
+		
+
+		if (point < data[mid] ) { end = mid; continue; }
+		if (data[mid] < point ) { begin = mid + 1; continue; }
+		if (shadow_data[mid] == '}') { end = mid; continue; }
+		if (shadow_data[mid] == '{') { begin = mid + 1; continue; }
+		
+	}
+	//cout << "  rmid: " << mid << endl;
+	return mid;
 }
 
 
@@ -239,19 +266,20 @@ void merge_sort(size_t **data, char **shadow_data, size_t length){
 int main(){
 	ifstream in("input.txt");
 	Queue_ myqeue;
-	size_t n, m, b,e;
+	int b,e;
+	size_t n, m;
 
 	in >> n >> m;
-	size_t *all = new size_t[n * 2 + m];//Тут все, и точки и концы отрезков.
+	int *all = new int[n * 2 + m];//Тут все, и точки и концы отрезков.
 	char *brackets = new char[n * 2 + m];//Графические обозначения точек и гнаниц.
 	size_t *count = new size_t[n * 2 + m];//Вложенность.
-	size_t *points = new size_t[m];//Точки.
+	int *points = new int[m];//Точки.
 	
 	for (size_t i = 0; i < n; i++){
 		in >> b >> e;
 		all[i * 2] = b;
 		all[i * 2 + 1] = e;
-		//myqeue.enqueue(all[i*2], all[i*2+1]);
+		
 		brackets[i * 2] = '{';
 		brackets[i * 2 + 1] = '}';
 	}
@@ -261,9 +289,16 @@ int main(){
 		brackets[i + 2 * n] = '.';
 	}
 
-	print(cout,"all", all, 2 * n + m);
-	print(cout, "brackets", brackets, 2 * n + m);
+	
 	merge_sort(&all,&brackets, n*2+m);
+	//cout << "       i: [";
+	//for (size_t i = 0; i < n * 2 + m-1; i++)
+		//cout << i << " ";
+	//cout <<2*n+m-1 << "]" << endl;
+	print(cout, "  points", points, m);
+	print(cout, "     all", all, 2 * n + m);
+	print(cout, "brackets", brackets, 2 * n + m);
+	
 
 	//Теперь подсчитаем вложенность для всех элементов.
 	size_t COUNT = 0;
@@ -272,15 +307,12 @@ int main(){
 	else if (brackets[i] == '}') count[i] = --COUNT;
 	else count[i] = COUNT;
 
-	print(cout, "count", count, 2 * n + m);
-	cout << endl << "p_count: ";
+	print(cout, "   count", count, 2 * n + m);
+	
+	//cout << endl << "p_count: ";
 	//Поиск по точкам.(Для начала не бинарный, ибо лень, авось пройдет.)
 	for (size_t i = 0; i < m; i++)
-		for (size_t j = 0; j < n * 2 + m; j++)
-		if (brackets[j] == '.' && all[j] == points[i]){
-			cout << count[j] << " ";
-			break;
-		}
+		cout << count[bin_search(all, brackets, n * 2 + m, points[i])] << " ";
 	
 	cout << endl;
 	
