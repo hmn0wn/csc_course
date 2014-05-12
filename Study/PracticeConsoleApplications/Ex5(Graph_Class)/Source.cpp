82328b8bb09e7c0ece8edbdb061cc4c28eccee5e
leadenHoar <eskendirrr@gmail.com>
2014-05-13 00:59:22 +0400

    Сдал задачу поиска цикла в ориентированном графе.

--- Study/PracticeConsoleApplications/Ex5(Graph_Class)/Source.cpp
+++ Study/PracticeConsoleApplications/Ex5(Graph_Class)/Source.cpp
@@ -41,12 +41,15 @@ public:
 	void explore(size_t v);
 	void dfs();
 	void clrvisited();
-	ostream &printadj(ostream & stream);
+	ostream &print_adjacency(ostream & stream);
+
+	void traverce_test();
+	void path_check_test();
 
 };
 
 void Graph::add(size_t xv, size_t yv){
-	
+
 	if (head){
 		node *tmp = head;
 		while (tmp->next){
@@ -55,8 +58,12 @@ void Graph::add(size_t xv, size_t yv){
 		}
 		if (tmp->first_vertice == xv && tmp->second_vertice == yv) return;
 		tmp->next = new node(xv, yv);
+		if (this->vertices_num < xv) this->vertices_num = xv;
+		if (this->vertices_num < yv) this->vertices_num = yv;
 	}
 	else head = new node(xv, yv);	
+	delete(visited);//Быдлокод.
+	visited = new bool[vertices_num];
 	edges_num++;
 }
 
@@ -72,6 +79,7 @@ void Graph::add(istream &in){
 	}
 	this->vertices_num = vn;
 	this->edges_num = en;
+
 	visited = new bool[vn];
 	fillarray(visited, false, vn);
 }
@@ -80,7 +88,8 @@ void Graph::explore(size_t v){
 	visited[v-1] = true;
 	node *tmp = head;
 	while (tmp){
-		if (tmp->first_vertice == v && !visited[tmp->second_vertice-1]) explore(tmp->second_vertice);//Ныряем если можно и если не былы там.
+		if (tmp->first_vertice == v && !visited[tmp->second_vertice-1]) 
+			explore(tmp->second_vertice);//Ныряем если можно и если не былы там.
 		tmp = tmp->next;
 	}
 	//Функция для ориентированного ребра, а при вводе для неоориентированного нужно продублировать.
@@ -98,41 +107,78 @@ void Graph::clrvisited(){
 	fillarray(visited, false, this->vertices_num + 1);
 }
 
-ostream &Graph::printadj(ostream &stream){
+ostream &Graph::print_adjacency(ostream &stream){
+	stream << endl << "adjacency list: " <<endl <<endl ;
 	node *tmp = head;
 	while (tmp){
 		stream << "(" << tmp->first_vertice << "," << tmp->second_vertice << ") - ";
 		tmp = tmp->next;
 	}
-	stream << "NULL" << endl;
+	stream << "NULL" << endl << "______________________________________________________________________________________________________________________" << endl;
 	return stream;
 }
 
+void Graph::traverce_test(){
+	cout << endl << "traverce test: " << endl ;
+	//Тестируем обход.
+	for (size_t i = 1; i < this->vertices_num+1; i++){
+		cout << endl<<"explore[" << i << "]: ";
+		this->clrvisited();
+		this->explore(i);
+		printarray(cout, this->visited, 0, this->vertices_num-1);
 
+	}
+	cout << endl << "______________________________________________________________________________________________________________________" << endl;
+}
+
+void Graph::path_check_test(){
+	//Тестируем проверку пути.	
+	cout << endl << "path check test: " <<endl;
+	for (size_t x = 1; x <= this->vertices_num; x++){
+		cout << endl;
+		for (size_t y = 1; y <= this->vertices_num; y++){
+			this->clrvisited();
+			this->explore(x);
+			cout << x << "->" << y << ": " << ((this->visited[y - 1]) ? "True" : "False") << endl;
+		}
+	}
+	cout << endl << "______________________________________________________________________________________________________________________" << endl;
+}
 
 
 int main(){
 	ifstream in("input.txt");
 	/*Graph mygraph;
-	for (size_t i = 0; i < 10; i++){
-		mygraph.add(1, 1);
-	}
-	mygraph.printadj(cout);*/
-	
+	for (size_t i = 1; i < 1001; i++){
+		mygraph.add(i, i+1);
+	}*/
+	//mygraph.print_adjacency(cout);
+	//mygraph.traverce_test();
+	//mygraph.path_check_test();
 
 	Graph mygraph_;
 	mygraph_.add(in);
-	//mygraph_.printadj(cout);
-
+	//mygraph_.print_adjacency(cout);
+	//mygraph_.traverce_test();
+	//mygraph_.path_check_test();
 	
-	/*for (size_t i = 1; i < mygraph_.vertices_num+1; i++){
-		cout << endl<<"explore[" << i << "]: ";
-		mygraph_.clrvisited();
-		mygraph_.explore(i);
-		printarray(cout, mygraph_.visited, 1, mygraph_.vertices_num);
-		
-	}*/
+	
 
-	mygraph_.dfs();
-	cout << mygraph_.connected_component_count << endl;
+
+
+	//Задача на проверку пути.
+	size_t x, y;
+	in >> x >> y;
+	mygraph_.clrvisited();
+	mygraph_.explore(x);
+	cout << ((mygraph_.visited[y - 1]) ? "True" : "False" );
+	
+
+
+
+
+	//Задача на связность.
+	//mygraph_.dfs();
+	//cout << mygraph_.connected_component_count << endl;
+	
 }
