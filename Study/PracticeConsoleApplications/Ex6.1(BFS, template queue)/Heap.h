766bc00ba4ac88ce8edfb4bf3d69e6f6621a2176
leadenHoar <eskendirrr@gmail.com>
2014-05-20 18:28:28 +0400

    Хочу подружить класс графа и очередь с приоритетами(кучу).

---
+++ Study/PracticeConsoleApplications/Ex6.1(BFS, template queue)/Heap.h
@@ -0,0 +1,194 @@
+
+//Задача одолела контест, но контейнер еще требует множества доработок.
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#define SIZE 100000
+
+using namespace std;
+
+
+
+class Heap{
+
+private:	
+	struct node{
+		size_t *key;
+		size_t data;
+		node(size_t *k, size_t d) : key(k), data(d){}
+		node() :key(0), data(0){}
+	};
+
+	node **heap;//
+	size_t max_length;
+
+public:
+	size_t length;
+
+	Heap(size_t size);
+	size_t data(size_t i);//
+	size_t key(size_t i);
+	size_t parent(size_t i);//
+	size_t leftc(size_t i);//
+	size_t rightc(size_t i);//
+
+	
+	void siftup(size_t i);//
+	void siftdown(size_t i);//
+	void swap(size_t i, size_t j);//	
+	void inserttail(size_t *k, size_t d);//
+	void insert(size_t *k, size_t d);//
+	size_t search(size_t *k, size_t d);
+	void changepriority(size_t *k, size_t v);
+	size_t extractmin();//
+	void del(size_t i);
+	void printheap();
+	
+
+	
+
+};
+
+Heap::Heap(size_t size) :length(0){
+	max_length = size;
+	//Так, мы создаем кучу на заранее известное колличество мест, 
+	//вставка не подразумевает создание новых объектов, только заполнение
+	//изначально созданных.
+
+	for (size_t i = 0; i<size; i++)
+		heap[i] = new node();
+}
+
+size_t Heap::data(size_t i){
+	return heap[i]->data;
+}
+
+size_t Heap::key(size_t i){
+	return heap[i]->key;
+}
+
+size_t Heap::parent(size_t i){
+	return (i / 2);
+}
+
+size_t Heap::leftc(size_t i){
+	return (2 * i);
+}
+
+size_t Heap::rightc(size_t i){
+	return (2 * i + 1);
+}
+
+
+void Heap::siftup(size_t i){
+	size_t pi = parent(i);
+	if (!pi) return;
+	if (key(pi) < key(i)) {
+		swap(i, pi);
+		siftup(pi);
+	}
+}
+
+void Heap::siftdown(size_t i){
+	size_t li = leftc(i);
+	size_t ri = rightc(i);
+
+	if (length == 1) return;
+	if (key(li) <= key(ri) && key(li) < key(i)){
+		swap(i, li);
+		siftdown(li);
+	}
+	else
+	if (key(ri) < key(li) && key(ri) < key(i)){
+		swap(i, ri);
+		siftdown(ri);
+	}
+}
+
+void Heap::swap(size_t i, size_t j){
+	node *tmp = heap[i];
+	heap[i] = heap[j];
+	heap[j] = tmp;
+
+
+	/*size_t tmp = data(j);
+	heap[j] = heap[i];
+	heap[i] = tmp;*/
+}
+
+void Heap::inserttail(size_t *k, size_t d){
+	heap[length]->key = k;
+	heap[++length]->data = d;
+
+}
+
+void Heap::insert(size_t *k, size_t d){
+	if (length == max_length){
+		cout << endl << "!!!HEAP OVERFLOW!!!" << endl;
+		return;
+	}
+	inserttail(k, d);
+	siftup(length);
+}
+
+size_t Heap::search(size_t *k, size_t d, size_t begin){
+	
+	
+}
+
+void Heap::changepriority(size_t *k, size_t v){
+	size_t i = 1, li, ri;
+	while (i <= length){
+		li = leftc(i);
+		ri = rightc(i);
+		if (heap[i]<heap[li])
+	}
+}
+
+size_t Heap::extractmin(){
+	node *tmp = heap[1];
+	heap[1] = heap[length];
+	heap[length]->data = 0;
+	heap[length]->key = 0;
+	length--;
+	siftdown(1);
+	return(tmp->data);
+}
+
+void Heap::printheap(){
+	cout << "___________________________" << endl << "[ ";
+	for (size_t i = 1; i <= length; i++)
+		cout << heap[i]->key << " : " << heap[i]->data << " ";
+	cout << "]" << endl;
+}
+
+
+
+/*
+int main(){
+	//ifstream in("input.txt");
+	//fstream out("output.txt");
+	Heap myheap;
+	size_t n, input;
+	char flag[10];
+	cin >> n;
+	for (size_t i = 0; i < n; i++){
+
+		cin >> flag;
+		if (!strcmp(flag, "Insert")){
+			cin >> input;
+			myheap.insert(input);
+		}
+		else cout << myheap.extract() << endl;
+	}
+
+
+	for (size_t i = 0; i < 100; i++)
+	myheap.insert(rand()%100 + 1);
+
+	for (size_t i = 0; i < myheap.getlength(); i++)
+	cout << myheap.extract() << endl;
+
+
+	return 0;
+}*/
--- Study/PracticeConsoleApplications/Ex6.1(BFS, template queue)/Source.cpp
+++ Study/PracticeConsoleApplications/Ex6.1(BFS, template queue)/Source.cpp
@@ -1,7 +1,9 @@
 #include <fstream>
 #include <iostream>
-using namespace std;
+#include <math.h>
 
+using namespace std;
+int MAX = powl(2, sizeof(int)* 8-1) - 1;
 
 
 template <class T>
@@ -19,96 +21,141 @@ ostream &printarray(ostream &stream, T *arr, char label[100], size_t begin, size
 	return stream;
 }
 
-#pragma region Queue_
-template <class T>
-class Queue_{
-private:
-	struct node{
-		T *data;
-		node *next;
-		node(T *d, node *n) :data(d), nest(n){}
-	};
-	T *head;
-	T *tail;
-	
-
-public:
-	size_t size;
-	
-	Queue_() : head(NULL), tail(NULL), size(0) {}
-	
-	void Inject(T *e);
-	T* Eject();
-	void clr();
-};
-template <class T>
-void Queue_::Inject(T *e){
-	
-	if (!this->tail){
-		tail = e;
-		head = e;
-	}
-	else{
-		
-	}
-}
-
-#pragma endregion 
-
 #pragma region Graph
 
 class Graph{
 private:
 	struct node{
 		size_t name;
+		int weight;
 		node *next;
-		node() : name(0), next(NULL){}
-		node(size_t nm, node *n) : name(nm), next(n){}
+		node() : name(0), weight(-1), next(NULL){}
+		node(size_t nm, int w, node *n) : name(nm), weight(w), next(n) {}
+	};
+
+	class Queue__{
+	private:
+		node *head;
+		node *tail;
+	public:
+		size_t size;
+
+		Queue__() :head(NULL), tail(NULL), size(0){}
+		void inject(size_t nm);
+		size_t eject();
+		void clr();
+		ostream &print(ostream &stream);
 	};
 
 public:
+	Queue__ Q;
+
 	size_t e_num;
 	size_t v_num;
 	node *vertices;
 	bool *visited;
+	int *dist;
+	size_t *prev;
 
-	Graph() :e_num(0), v_num(0),  vertices(NULL), visited(NULL){}
+	Graph() :e_num(0), v_num(0),  vertices(NULL), visited(NULL), dist(NULL), prev(NULL) {}
 
-	void add(node *adj_array, size_t xv, size_t yv);
-	void add(istream &in, node *adj_array);
+	void add(node *adj_array, size_t xv, size_t yv, int w);
+	void add(istream &in, node **adj_array);
 
-	void explore(size_t v, node* adj_array);
-	void dfs(node *adj_array);	
+	void explore(size_t v, node *adj_array);
+	void dfs(node *adj_array);
+	void bfs(node *adj_array, size_t s);
+	void relax(size_t u, size_t v);
+	int weight(size_t u, size_t v);
 	
 	void clrvisited();
 	ostream &print_adjacency(ostream &stream, node *adj_array);
 	
 };
 
-void Graph::add(node *adj_array, size_t xv, size_t yv){
+void Graph::Queue__::inject(size_t nm){
+	if (!head){
+		tail = new node(nm, -1, NULL);
+		head = tail;
+	}
+	else{
+		tail->next = new node(nm, -1, NULL);
+		tail = tail->next;
+	}
+
+}
+
+size_t Graph::Queue__::eject(){
+	
+	if (head){
+		size_t r = head->name;
+		
+		if (head->next){
+			node *tmp = head->next;
+			delete(head);
+			head = tmp;			
+		}
+		else{
+			delete(head);
+			head = NULL;
+			tail = NULL;
+		}
+		
+		return r;
+	}
+	return 0;
+}
+
+void Graph::Queue__::clr(){
+	while (eject() != 0){}
+}
+
+ostream &Graph::Queue__::print(ostream &stream){
+	stream << endl << "queue: " << endl << endl;
+	node *tmp = head;
+	while (tmp){
+		stream << "(" << tmp->name << ") - ";
+		tmp = tmp->next;
+	}
+	stream << "NULL" << endl;
+	stream << "______________________________________________________________________________________________________________________" << endl;
+	return stream;
+}
+
+void Graph::add(node *adj_array, size_t xv, size_t yv, int w){
+
 	node *tmp = &adj_array[xv];
 	while (tmp->next && tmp->next->name < yv)
 		tmp = tmp->next;
-	tmp->next = new node(yv, tmp->next);//Вставляем сразу на нужное место
+	tmp->next = new node(yv, w, tmp->next);//Вставляем сразу на нужное место
 }
 
-void Graph::add(istream &in, node *adj_array){
-	size_t xv, yv, vn, en;
+void Graph::add(istream &in, node **adj_array){
+	size_t vn, en;
 	in >> vn >> en;
 
-	adj_array = new node[vn + 1];
+	*adj_array = new node[vn + 1];
 	this->v_num = vn;
 	this->e_num = en;
 
 	for (size_t i = 1; i <= vn; i++)
-		adj_array[i].name = i;
+		(*adj_array)[i].name = i;
 
 	visited = new bool[vn + 1];
 	fillarray(visited, false, vn + 1);
 
+	dist = new int[vn + 1];
+	fillarray<int>(dist, -1, vn + 1);
+
+	prev = new size_t[vn + 1];
+	fillarray<size_t>(prev, 0, vn + 1);
+
+	size_t xv, yv;
+	int w;
 	for (size_t i = 0; i < en; i++){
-		in >> xv >> yv;
-		this->add(adj_array, xv, yv);
+		in >> xv >> yv >> w;
+		this->add(*adj_array, xv, yv, w);
+		//this->add(*adj_array, yv, xv);//Неориентированный.
 	}
 
 }
@@ -134,6 +181,51 @@ void Graph::dfs(node *adj_array){
 	if (!visited[i]) {explore(i, adj_array); }
 }
 
+void Graph::bfs(node *adj_array, size_t s){
+	
+	dist[s] = 0;
+	Q.inject(s);
+	
+	node *tmp;
+	size_t u, v;
+	while ((u = Q.eject()) != 0){
+		tmp = &vertices[u];
+		while (tmp->next){
+			tmp = tmp->next;
+			v = tmp->name;
+
+			relax(u, v);
+			//Просто поиск в ширину для невзвешенного графа.
+			/*if (dist[v] == -1){
+				Q.inject(v);
+				dist[v] = dist[u]+1;
+			}*/
+		}
+	}
+}
+
+void Graph::relax(size_t u, size_t v){
+	int w = weight(u, v);
+	//cout << "relax: (" << u << ") -> (" << v << ") [" << w <<"]" << endl;
+	if (dist[v]== -1 || dist[v] > dist[u] + w) {
+		Q.inject(v);
+		dist[v] = dist[u] + w;
+		prev[v] = u;
+		
+		//Q.print(cout);	
+	}
+		
+}
+
+int Graph::weight(size_t u, size_t v){
+	node *tmp = &vertices[u];
+	while (tmp->next){
+		tmp = tmp->next;
+		if (tmp->name == v) return tmp->weight;
+	}
+	return -1;
+}
+
 void Graph::clrvisited(){
 	fillarray(visited, false, this->v_num + 1);
 }
@@ -141,7 +233,7 @@ void Graph::clrvisited(){
 ostream &Graph::print_adjacency(ostream &stream, node *adj_array){
 	stream << endl << "adjacency list: " << endl << endl;
 	node *tmp;
-	for (size_t v = 1; v <= this->v_num; v++){
+	for (size_t v = 1;(adj_array) && (v <= this->v_num); v++){
 		tmp = &adj_array[v];
 		while (tmp){
 			stream << "(" << tmp->name << ") - ";
@@ -155,10 +247,12 @@ ostream &Graph::print_adjacency(ostream &stream, node *adj_array){
 
 #pragma endregion
 
+
+
 int main(){
 
 	ifstream in("input.txt");
-	ofstream out("output.txt");
+	
 
 	//Рандомное заполнение графа.
 	/*size_t n = 100;
@@ -167,9 +261,24 @@ int main(){
 	out << (rand() % n) +1 << " " << (rand() % n) +1 << endl;*/
 
 	Graph g;
-	g.add(in, g.vertices);
-
+	g.add(in, &g.vertices);//Разберись с сылками.
 	//g.print_adjacency(cout, g.vertices);	
+
+	//for (size_t i = 1; i < g.v_num; i++)
+	//	g.Q.inject(i);
+	
+	//g.Q.print(cout);
+	//g.Q.clr();
+	//g.Q.print(cout);
 	//g.dfs();
+
+	size_t s, t;
+	in >> s >> t;
+
+	g.bfs(g.vertices, s);
+
+	//printarray(cout, g.dist, "dist", 1, g.v_num);
+	cout << /*(g.dist[t] != -1 ? g.dist[t] : -1)*/g.dist[t] << endl;
+	
 	
 }
